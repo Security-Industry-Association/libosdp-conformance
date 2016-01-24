853a2500a21104193425c6d1dadd05dbdd41e46b
Rodney Thayer <rodney+github@smithee.us>
2016-01-24 09:41:13 -0800

    q-a initiator

--- src-tls/initiator.c
+++ src-tls/initiator.c
@@ -2,7 +2,32 @@
   initiator - re-runs a service repeatedly
 
   (because cleaning up the init code for TLS was more work.)
+
+  (C)Copyright 2014-2015 Smithee Spelvin Agnew & Plinge, Inc.
+
+  Support provided by the Security Industry Association
+  http://www.securityindustry.org
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define EQUALS ==
+
 
 int
   main
@@ -15,6 +40,8 @@ int
 
   int
     done;
+  FILE
+    *ef;
   int
     status;
 
