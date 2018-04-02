3762b1a6528af17036d71c4878142f2153317827
Rodney Thayer <github@smithee.solutions>
2018-04-02 06:11:00 -0700

    0.2.15

--- src-tls/osdp-local-config.h
+++ src-tls/osdp-local-config.h
@@ -1,5 +1,24 @@
 /*
   osdp-local-config.h - local (platform) configuration values
+
+  (C)Copyright 2014-2017 Smithee,Spelvin,Agnew & Plinge, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  Support provided by the Security Industry Association
+  http://www.securityindustry.org
+*/
+
 */
 
 // the server key and cert for the OSDP TLS server
