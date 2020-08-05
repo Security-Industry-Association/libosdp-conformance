efad34ca927360187e17c3eb5bf1ce9326f2a6c2
Rodney Thayer <github@smithee.solutions>
2020-08-05 08:11:42 -0700

    split out iec-nak.h

---
+++ include/iec-nak.h
@@ -0,0 +1,13 @@
+// NAK response codes for use with IEC 60839-11-5 and beyond
+
+// NAK error codes
+#define OO_NAK_CHECK_CRC            (1)
+#define OO_NAK_COMMAND_LENGTH       (2)
+#define OO_NAK_UNK_CMD              (3)
+#define OO_NAK_SEQUENCE             (4)
+#define OO_NAK_UNSUP_SECBLK         (5)
+#define OO_NAK_ENC_REQ              (6)
+#define OO_NAK_BIO_TYPE_UNSUPPORTED (7)
+#define OO_NAK_BIO_FMT_UNSUPPORTED  (8)
+#define OO_NAK_CMD_UNABLE           (9)
+
