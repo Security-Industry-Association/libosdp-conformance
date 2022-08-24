1037dac96da6bab92d17f2a4a4c109872d091edd
Rodney Thayer <github@smithee.solutions>
2022-08-24 08:00:38 -0700

    roll the version to 1.28-1

--- include/open-osdp.h
+++ include/open-osdp.h
@@ -28,8 +28,8 @@
 #endif
 
 #define OSDP_VERSION_MAJOR ( 1)
-#define OSDP_VERSION_MINOR (27)
-#define OSDP_VERSION_BUILD ( 4)
+#define OSDP_VERSION_MINOR (28)
+#define OSDP_VERSION_BUILD ( 1)
 
 #define OSDP_EXCLUSIVITY_LOCK "/opt/osdp-conformance/run/osdp-lock"
 #define OSDP_SAVED_PARAMETERS    "osdp-saved-parameters.json"
